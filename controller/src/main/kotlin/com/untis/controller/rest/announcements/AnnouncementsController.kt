2b4318760d7f6b7210272a9e1845cdb580dde872
simon <simon@bumiller.me>
2024-05-31 20:18:32 +0200

    create global announcements controller

---
+++ controller/src/main/kotlin/com/untis/controller/rest/announcements/AnnouncementsController.kt
@@ -0,0 +1,44 @@
+package com.untis.controller.rest.announcements
+
+import com.untis.controller.base.ControllerScope
+import com.untis.controller.body.response.announcement.AnnouncementAttachmentResponse
+import com.untis.controller.validating.validateAnnouncementMessageExists
+import com.untis.model.AnnouncementAttachmentMeta
+import com.untis.service.AnnouncementAttachmentService
+import com.untis.service.AnnouncementMessageService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+
+@RestController
+@RequestMapping("/announcements/")
+class AnnouncementsController @Autowired constructor(
+
+    override val announcementMessageService: AnnouncementMessageService,
+
+    override val announcementAttachmentService: AnnouncementAttachmentService
+
+) : ControllerScope() {
+
+    @PostMapping("{id}/attachments/", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun addAttachment(
+        @PathVariable id: Long,
+        @RequestParam("attachmentFile") attachmentFile: MultipartFile
+    ): AnnouncementAttachmentResponse {
+        validateAnnouncementMessageExists(id)
+
+        val model = AnnouncementAttachmentMeta(
+            id = null,
+            filename = attachmentFile.name,
+            size = attachmentFile.size
+        ).let {
+            announcementAttachmentService.create(it, id, attachmentFile)
+        }
+
+
+
+        return AnnouncementAttachmentResponse.create(model)
+    }
+
+}
