4df89c1350bab754fd26fca0e5905df801df34b2
simon <simon@bumiller.me>
2024-06-03 21:56:33 +0200

    fix wrong password hash comparison

--- service/src/main/kotlin/com/untis/service/impl/UserSecurityServiceImpl.kt
+++ service/src/main/kotlin/com/untis/service/impl/UserSecurityServiceImpl.kt
@@ -67,7 +67,7 @@ internal class UserSecurityServiceImpl @Autowired constructor(
     override fun verifyUserCredentials(email: String, password: String): User? = userService
         .getByEmail(email)
         .let { user ->
-            val valid = user?.encodedPassword == passwordEncoder.encode(password)
+            val valid = passwordEncoder.matches(password, user?.encodedPassword ?: "")
 
             if (!valid || user == null) null
             else user
